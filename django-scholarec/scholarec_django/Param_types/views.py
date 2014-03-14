d549e074579f28838f66a74d9f4b4f64734fe676
arcolife <arcshams@gmail.com>
2014-03-14 05:47:43 +0530

    add social app; repo cleanup

--- django-scholarec/scholarec_django/Param_types/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render_to_response
-
-def home(request):
-    d = {}
-    return render_to_response('home.html', d)
-
-#def index(request):    
-#    return render_to_response('index.html', {})
--- django-scholarec/scholarec_django/scholarec/tests/run-tests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-grep '<entry>' ./log/output/query_results.xml | wc -l
-python ./scholarec/__init__.py
-python ./scholarec/Base/DocumentClass.py
