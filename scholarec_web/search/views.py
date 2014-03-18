635ae0d68ead0b9a11cff466a4756aa902f24880
arcolife <arcshams@gmail.com>
2014-03-19 00:19:05 +0530

    handle csrd in django

--- scholarec_web/search/views.py
+++ scholarec_web/search/views.py
@@ -1,12 +1,15 @@
+from django.core.context_processors import csrf
 from django.shortcuts import render_to_response
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 
 def search(request):
+    c = {}
+    c.update(csrf(request))
     if request.POST:
         print request.POST['q']
         return HttpResponseRedirect("/")
     else:
-        return render_to_response('search.html', {})
+        return render_to_response('search.html', c)
 
 def test(request):    
     return render_to_response('test.html', {})
