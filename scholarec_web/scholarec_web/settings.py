b303ea31aa9873bb0c4b945549aebd4315b877b3
arcolife <arcshams@gmail.com>
2014-05-05 03:51:51 +0530

    add panels to /results; refactor /login

--- scholarec_web/scholarec_web/settings.py
+++ scholarec_web/scholarec_web/settings.py
@@ -132,7 +132,7 @@ INSTALLED_APPS = (
     'social.apps.django_app.default',
     'scholarec_web.app',
     'django_facebook',
-    'django_extensions',
+    #'django_extensions',
 )
 
 HAYSTACK_CONNECTIONS = {
@@ -272,11 +272,11 @@ READABILITY_CONSUMER_SECRET  = ''
 #AUTH_USER_MODEL = 'django_facebook.FacebookCustomUser'
 AUTH_PROFILE_MODULE = 'django_facebook.FacebookProfile'
 
-FACEBOOK_APP_ID = 226953304169624 #os.environ.get('FB_KEY_L')
-FACEBOOK_APP_SECRET = '7e7cb2fc70350e055ab9f09fdeb8aa2a' #os.environ.get('FB_SECRET_L')
+FACEBOOK_APP_ID = os.environ.get('FB_KEY_L')
+FACEBOOK_APP_SECRET = os.environ.get('FB_SECRET_L')
 
-SOCIAL_AUTH_FACEBOOK_APP_ID = 226953304169624 #os.environ.get('FB_KEY_L')
-SOCIAL_AUTH_FACEBOOK_SECRET = '7e7cb2fc70350e055ab9f09fdeb8aa2a' #os.environ.get('FB_SECRET_L')
+SOCIAL_AUTH_FACEBOOK_APP_ID = os.environ.get('FB_KEY_L')
+SOCIAL_AUTH_FACEBOOK_SECRET = os.environ.get('FB_SECRET_L')
 #SOCIAL_AUTH_FACEBOOK_APP_NAMESPACE = 'scholarec'
 #SOCIAL_AUTH_FACEBOOK_EXTENDED_PERMISSIONS = ['email']
 
